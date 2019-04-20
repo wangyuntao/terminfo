787fb461f5a7e322c93a550ee3d2315013e19e71
wangyuntao <wyt.daily@gmail.com>
2019-04-20 20:18:03 +0800

    Add utility functions related to display attributes

---
+++ _example/attr.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/wangyuntao/terminfo"
+)
+
+func main() {
+	ti, err := terminfo.LoadEnv()
+	failIfErr(err)
+
+	err = standout(ti)
+	failIfErr(err)
+
+	err = underline(ti)
+	failIfErr(err)
+
+	err = reverse(ti)
+	failIfErr(err)
+
+	err = blink(ti)
+	failIfErr(err)
+
+	err = dim(ti)
+	failIfErr(err)
+
+	err = bold(ti)
+	failIfErr(err)
+}
+
+func standout(ti *terminfo.Terminfo) error {
+	err := ti.EnterStandoutMode()
+	if err != nil {
+		return err
+	}
+	fmt.Println("standout")
+	return ti.ExitAttributeMode()
+}
+
+func underline(ti *terminfo.Terminfo) error {
+	err := ti.EnterUnderlineMode()
+	if err != nil {
+		return err
+	}
+	fmt.Println("underline")
+	return ti.ExitAttributeMode()
+}
+
+func reverse(ti *terminfo.Terminfo) error {
+	err := ti.EnterReverseMode()
+	if err != nil {
+		return err
+	}
+	fmt.Println("reverse")
+	return ti.ExitAttributeMode()
+}
+
+func blink(ti *terminfo.Terminfo) error {
+	err := ti.EnterBlinkMode()
+	if err != nil {
+		return err
+	}
+	fmt.Println("blink")
+	return ti.ExitAttributeMode()
+}
+
+func dim(ti *terminfo.Terminfo) error {
+	err := ti.EnterDimMode()
+	if err != nil {
+		return err
+	}
+	fmt.Println("dim")
+	return ti.ExitAttributeMode()
+}
+
+func bold(ti *terminfo.Terminfo) error {
+	err := ti.EnterBoldMode()
+	if err != nil {
+		return err
+	}
+	fmt.Println("bold")
+	return ti.ExitAttributeMode()
+}
+
+func failIfErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ attr.go
@@ -0,0 +1,29 @@
+package terminfo
+
+func (ti *Terminfo) EnterStandoutMode() error {
+	return ti.Do(EnterStandoutMode)
+}
+
+func (ti *Terminfo) EnterUnderlineMode() error {
+	return ti.Do(EnterUnderlineMode)
+}
+
+func (ti *Terminfo) EnterReverseMode() error {
+	return ti.Do(EnterReverseMode)
+}
+
+func (ti *Terminfo) EnterBlinkMode() error {
+	return ti.Do(EnterBlinkMode)
+}
+
+func (ti *Terminfo) EnterDimMode() error {
+	return ti.Do(EnterDimMode)
+}
+
+func (ti *Terminfo) EnterBoldMode() error {
+	return ti.Do(EnterBoldMode)
+}
+
+func (ti *Terminfo) ExitAttributeMode() error {
+	return ti.Do(ExitAttributeMode)
+}
